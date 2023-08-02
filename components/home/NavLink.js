13d6e18de09d8bc648caec734e6b292503a4cf37
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-02 17:07:12 +0700

    update responsive

--- components/home/NavLink.js
+++ components/home/NavLink.js
@@ -22,15 +22,17 @@ const NavLinkHome = () => {
   ];
 
   return (
-    <nav className="flex flex-col md:flex-row justify-center items-center gap-2 mt-10 ">
+    <nav className="flex flex-col md:flex-row justify-center items-center gap-2 mt-10 w-[80%]">
       {NavLink.map((e, i) => {
         return (
           <Link
             href={e.href}
             key={i}
-            className="flex flex-col justify-center items-center border-2 border-blue-800 bg-blue-100 rounded-lg text-center w-[300px] h-[100px]"
+            className="flex flex-col justify-center items-center border-2 border-blue-800 bg-blue-100 rounded-lg text-center py-3 w-full md:w-[200px] lg:w-[300px]"
           >
-            <h1 className="font-bold text-lg">{e.h1}</h1>
+            <h1 className="font-bold text-[.5rem] md:text-sm ld:text-lg whitespace-nowrap">
+              {e.h1}
+            </h1>
             <p className="text-4xl font-bold">{e.p}</p>
           </Link>
         );
