83307f2125e30ef5ba23bce8652b0866b9839ff8
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-07 06:54:11 +0700

    update css and text content

--- src/components/home/NavHome.js
+++ src/components/home/NavHome.js
@@ -30,7 +30,7 @@ const NavHome = () => {
             key={i}
             className="flex flex-col justify-center items-center border-2 border-blue-800 bg-blue-100 rounded-lg text-center py-3 w-full md:w-[200px] lg:w-[300px]"
           >
-            <h1 className="font-bold text-[.5rem] md:text-sm ld:text-lg whitespace-nowrap">
+            <h1 className="text-sm font-bold ld:text-lg whitespace-nowrap">
               {e.h1}
             </h1>
             <p className="text-4xl font-bold">{e.p}</p>
