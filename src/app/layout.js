8c3cddb73dfc88bca5f04e15e36f8ab1f4b8a3a1
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-07 06:53:27 +0700

    add font
    add Orbitron font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,14 @@
 import "../../node_modules/tailwindcss/tailwind.css";
+import { Orbitron } from "@next/font/google";
+
+const font = Orbitron({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${font.className}`}>
       <head />
       <body>{children}</body>
     </html>
