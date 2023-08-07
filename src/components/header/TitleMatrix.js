e5646800926cc089383ab54b529a396fcd86c843
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-07 07:01:35 +0700

    update size responsive

--- src/components/header/TitleMatrix.js
+++ src/components/header/TitleMatrix.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const TitleMatrix = (props) => {
   return (
-    <div className="hidden w-1/3 font-mono font-bold text-center sm:block sm:text-3xl md:text-4xl lg:text-5xl text-slate-100">
+    <div className="hidden w-1/3 font-bold text-center sm:block sm:text-2xl md:text-3xl lg:text-4xl text-slate-100">
       {props.title}
     </div>
   );
