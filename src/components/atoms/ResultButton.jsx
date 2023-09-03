452f072d32846c0b19c1f62a5f465bf59a7358a0
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-03 19:07:28 +0700

    new component

---
+++ src/components/atoms/ResultButton.jsx
@@ -0,0 +1,14 @@
+const ResultButton = ({ funcLogic }) => {
+  return (
+    <button
+      onClick={() => {
+        funcLogic();
+      }}
+      className="btn bg-blue-900 hover:bg-blue-800 px-4 py-0.5 rounded-md cursor-pointer active:bg-slate-800 text-3xl text-white border border-blue-700 w-full text-center my-2"
+    >
+      RESULT
+    </button>
+  );
+};
+
+export default ResultButton;
