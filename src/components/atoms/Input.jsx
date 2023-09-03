be84865fe5953ce90c14f131289793a788d90e46
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-03 07:00:17 +0700

    new Input component

---
+++ src/components/atoms/Input.jsx
@@ -0,0 +1,10 @@
+const Input = ({ defaultValue }) => {
+  return (
+    <input
+      defaultValue={defaultValue}
+      className="PROCESS__INPUT border border-black w-[50px] rounded-lg text-center text-xl font-semibold "
+    />
+  );
+};
+
+export default Input;
