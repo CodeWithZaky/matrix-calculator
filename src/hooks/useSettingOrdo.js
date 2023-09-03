21fa349cc8db9e39eb9329d544601a7c76cdf183
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-03 07:04:45 +0700

    create useSettingOrdo
    for setting ordo

---
+++ src/hooks/useSettingOrdo.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Swal from "sweetalert2";
+
+// Custom hook untuk validasi input ordo
+function useSettingOrdo(initialRow = 3, initialCol = 3) {
+  const [row, setRow] = useState(initialRow);
+  const [col, setCol] = useState(initialCol);
+
+  const handleSettingOrdo = (e) => {
+    e.preventDefault();
+
+    const [rowValue, colValue] = e.target;
+    const newRow = parseInt(rowValue.value);
+    const newCol = parseInt(colValue.value);
+
+    if (isNaN(newRow) || isNaN(newCol) || newRow < 1 || newCol < 1) {
+      Swal.fire({
+        icon: "error",
+        title: "Masukan tidak valid. Harus angka lebih dari 0.",
+      });
+      return;
+    }
+
+    setRow(newRow);
+    setCol(newCol);
+  };
+
+  return { row, col, handleSettingOrdo };
+}
+
+export default useSettingOrdo;
