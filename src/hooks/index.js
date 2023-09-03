829539bc915507f3713dc72e82503e78377eb04a
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-03 07:02:15 +0700

    base import all hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,17 @@
+import useAddition from "./useAddition";
+import useMultiplication from "./useMultiplication";
+import useSubtraction from "./useSubtraction";
+import useSettingOrdo from "./useSettingOrdo";
+import useCreateOrdo from "./useCreateOrdo";
+import useMatrixForm from "./useMatrixForm";
+import useResult from "./useResult";
+
+export {
+  useAddition,
+  useSettingOrdo,
+  useMultiplication,
+  useSubtraction,
+  useCreateOrdo,
+  useMatrixForm,
+  useResult,
+};
