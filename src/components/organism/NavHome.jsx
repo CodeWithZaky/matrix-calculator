e005797476bf928e1970756031fc41c30b09e143
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-07 09:24:32 +0700

    change directory and update semantic variable

--- src/components/organism/NavHome.jsx
+++ src/components/organism/NavHome.jsx
@@ -5,34 +5,34 @@ const NavHome = () => {
   const NavLink = [
     {
       href: "matrix-addition",
-      h1: "matrix addition",
-      p: "+",
+      label: "matrix addition",
+      symbol: "+",
     },
     {
       href: "matrix-subtraction",
-      h1: "matrix subtraction",
-      p: "-",
+      label: "matrix subtraction",
+      symbol: "-",
     },
     {
       href: "matrix-multiplication",
-      h1: "matrix multiplication",
-      p: "x",
+      label: "matrix multiplication",
+      symbol: "x",
     },
   ];
 
   return (
     <nav className="flex flex-col md:flex-row justify-center items-center gap-2 mt-10 w-[80%]">
-      {NavLink.map((e, i) => {
+      {NavLink.map((item, index) => {
         return (
           <Link
-            href={e.href}
-            key={i}
+            href={item.href}
+            key={index}
             className="flex flex-col justify-center items-center border-2 border-blue-100 bg-blue-900 rounded-lg text-center py-3 w-full md:w-[200px] lg:w-[300px]"
           >
             <h1 className="text-sm font-bold ld:text-lg whitespace-nowrap text-slate-50">
-              {e.h1}
+              {item.label}
             </h1>
-            <p className="text-4xl font-bold text-slate-50">{e.p}</p>
+            <p className="text-4xl font-bold text-slate-50">{item.symbol}</p>
           </Link>
         );
       })}
