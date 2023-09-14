25d98fad3926190dde0d6439ef96fe6a7a1d6ef8
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-14 20:31:58 +0700

    update path import

--- src/hooks/useCreateOrdo.js
+++ src/hooks/useCreateOrdo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Input from "../components/atoms/Input";
+import Input from "../components/elements/Input";
 
 function useCreateOrdo(row, col) {
   const grid = Array.from({ length: row }, (_, rowIndex) => (
