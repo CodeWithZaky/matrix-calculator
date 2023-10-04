8e90106a5f31aa4aa1179240cded517eab507441
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-10-04 09:08:47 +0700

    update root layout for toast sonner layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import "../../node_modules/tailwindcss/tailwind.css";
 import { Orbitron } from "@next/font/google";
 import HeaderFooterWrap from "../components/layouts/HeaderFooterWrap";
+import { Toaster } from "sonner";
 
 const font = Orbitron({
   weight: "400",
@@ -10,8 +11,11 @@ const font = Orbitron({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${font.className}`}>
-      <body className="flex flex-col items-center justify-between min-h-screen gap-3 bg-gray-300 min-w-screen">
-        <HeaderFooterWrap>{children}</HeaderFooterWrap>
+      <body>
+        <Toaster position="top-right" richColors />
+        <div className="flex flex-col items-center justify-between min-h-screen gap-3 bg-gray-300/90 min-w-screen">
+          <HeaderFooterWrap>{children}</HeaderFooterWrap>
+        </div>
       </body>
     </html>
   );
