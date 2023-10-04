d4f413009887cc92cf6dcf041d955011a2ff185c
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-10-04 09:11:19 +0700

    toas sonner implementation

--- src/components/elements/SubmitButtton.jsx
+++ src/components/elements/SubmitButtton.jsx
@@ -1,8 +1,10 @@
 import { clsx } from "clsx";
+import { toast } from "sonner";
 
-const SubmitButtton = ({ label, layout }) => {
+const SubmitButtton = ({ label, title, layout }) => {
   return (
     <button
+      onClick={() => toast.success(`matrix ${title} submitted`)}
       type="submit"
       className={clsx(
         "rounded-md btn text-white bg-blue-900 hover:bg-blue-800 active:bg-slate-400",
