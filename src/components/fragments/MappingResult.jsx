7395ded6abb885911d8c57b470cc7757628c5c76
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-10-21 07:11:31 +0700

    update responsive css

--- src/components/fragments/MappingResult.jsx
+++ src/components/fragments/MappingResult.jsx
@@ -1,12 +1,12 @@
 const MappingResult = (props) => {
   return (
-    <div className="flex flex-col items-start">
+    <div className="flex flex-col justify-center items-center">
       {props.results.map((m, n) => (
-        <div key={n} className="flex">
+        <div key={n} className="flex justify-center items-center">
           {m.map((x, y) => (
             <div
               key={y}
-              className=" w-[50px] flex justify-center items-center border border-black rounded-lg text-xl font-semibold bg-white"
+              className=" min-w-[50px] flex justify-center items-center border border-black rounded-lg text-xl font-semibold bg-white"
             >
               {x}
             </div>
